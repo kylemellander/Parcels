ce955e544d97209faf4802942262e2ba933bb010
Andrew Dang & Kyle Mellander <student@epicodus.com>
2015-08-10 11:40:33 -0700

    returns shipping cost for 1 delivery day

--- lib/parcel.rb
+++ lib/parcel.rb
@@ -10,8 +10,11 @@ class Parcel
     @length.*(@width).*(@height)
   end
 
-  def weight
-    @weight
+  def cost_to_ship(delivery_type, distance)
+    if delivery_type.==("1 Day")
+      cost = '%.2f' % ((volume().to_f * 0.02 + @weight.to_f() * 0.50) * distance.to_f * 0.05)    
+    end
+    cost.to_s.prepend("$")
   end
 
 end
--- spec/parcel_spec.rb
+++ spec/parcel_spec.rb
@@ -9,10 +9,10 @@ describe(Parcel) {
     }
   }
 
-  describe('#weight') {
-    it('returns weight of the parcel') {
+  describe('#cost_to_ship') {
+    it('returns the cost to ship an item for one day shipping') {
       parcel = Parcel.new(2,3,4,6)
-      expect(parcel.weight).to(eq(6))
+      expect(parcel.cost_to_ship("1 Day", 100)).to(eq("$17.40"))
     }
   }
 
